96a1dc5177a1d44b05a0ee832195196bbf70ae6d
Blagoja Ovezoski <blagojaovezoski@gmail.com>
2025-06-30 13:08:48 +0200

    CI tags, enironment configuration, pagination class

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 
