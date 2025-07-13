0b08fc9446c6b20782a6eb4a87720ea5606e793d
Blagoja Ovezoski <blagojaovezoski@gmail.com>
2025-07-13 10:52:52 +0200

    Spectacular Docs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,6 +8,12 @@ from django.conf.urls.static import static
 from profiles.serializers import ProfileSerializer
 from profiles.views import ProfileViewSet
 from property.urls import router as property_router
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -41,6 +47,17 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("prometheus/", include("django_prometheus.urls")),
     path("silk/", include("silk.urls", namespace="silk")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + debug_toolbar_urls()
 
 if settings.DEBUG:
--- backend/property/views.py
+++ backend/property/views.py
@@ -9,16 +9,16 @@ from django.utils.decorators import method_decorator
 class StandardResultSetPagination(pagination.PageNumberPagination):
     page_size = 8
     max_page_size = 100
-    page_query_param = 'page'
-    page_size_query_param = 'pageSize'
+    page_query_param = "page"
+    page_size_query_param = "pageSize"
 
 
-@method_decorator(ensure_csrf_cookie, name='dispatch')
+@method_decorator(ensure_csrf_cookie, name="dispatch")
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['name']
+    search_fields = ["name"]
     pagination_class = StandardResultSetPagination
 
     def get_serializer_context(self):
@@ -26,15 +26,16 @@ class PropertyViewSet(viewsets.ModelViewSet):
         Passes the request object to the serializer context,
         which is necessary for `request.build_absolute_uri`.
         """
-        return {'request': self.request}
+        return {"request": self.request}
 
     def get_permissions(self):
         """
-        Instantiates and returns the list of permissions that this view requires.
+        Instantiates and returns the list of
+        permissions that this view requires.
         """
-        if self.action == 'create':
+        if self.action == "create":
             permission_classes = [permissions.AllowAny]
-        elif self.action in ['update', 'partial_update', 'destroy', 'delete']:
+        elif self.action in ["update", "partial_update", "destroy", "delete"]:
             permission_classes = [permissions.AllowAny]
         else:
             permission_classes = [permissions.AllowAny]
